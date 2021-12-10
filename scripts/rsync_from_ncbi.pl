4f648f5a83498f666193be7b9b71a38e3b99ee24
Jen Lu <jennifer.lu717@gmail.com>
2021-12-10 16:01:45 -0500

    Update rsync_from_ncbi.pl
    Fix expected path for ncbi ftp site

--- scripts/rsync_from_ncbi.pl
+++ scripts/rsync_from_ncbi.pl
@@ -43,7 +43,7 @@ while (<>) {
   my $full_path = $ftp_path . "/" . basename($ftp_path) . $suffix;
   # strip off server/leading dir name to allow --files-from= to work w/ rsync
   # also allows filenames to just start with "all/", which is nice
-  if (! ($full_path =~ s#^ftp://${qm_server}${qm_server_path}/##)) {
+  if (! ($full_path =~ s#^https://${qm_server}${qm_server_path}/##)) {
     die "$PROG: unexpected FTP path (new server?) for $ftp_path\n";
   }
   $manifest{$full_path} = $taxid;
